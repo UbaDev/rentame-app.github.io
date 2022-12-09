ab6ef925b5369d930aa1153c0ecc30f5c21b49d4
Ubaldo Medina Aviles <ubaldoaviles1@gmail.com>
2022-12-08 23:37:25 -0600

    apk

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,8 +9,8 @@
     <div class="container d-flex flex-column align-items-center" data-aos="zoom-in" data-aos-delay="100">
       <h1 class="text-white">Rentame</h1>
       <h2 class="text-white opacity-50">Descubre las mejores propiedades en renta de la ciudad</h2>
-      <router-link style="background-color:#8b3dff; text-decoration:none;" class="btn-about" v-if="!user" :to="{ name: 'Login' }">Comenzar
-      </router-link>
+      <a href="https://expo.dev/artifacts/72f2ad6c-c523-4ebf-86aa-db5a46238484" style="background-color:#8b3dff; text-decoration:none; padding:15px 25px; border-radius:10px; cursor:pointer" class="btn-about">Descargar APK
+      </a>
     </div>
   </section><!-- End Hero -->
  
